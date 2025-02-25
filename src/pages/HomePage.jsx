35c0c018ebe635ad9b2bfb8637eaab6f38255479
galdoben <galdobenedetta@gmail.com>
2025-02-25 11:43:02 +0100

    feat: style-home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,25 +13,30 @@ const HomePage = () => {
   }, [])
 
   return (
-    <div className="flex flex-wrap gap-4 justify-center mt-10">
-      {apartments.map(apartment => (
+    <div className="flex flex-wrap justify-center gap-6 mt-10">
+      {apartments.map((apartment) => (
+        <div key={apartment.id} className="w-80">
+          <div className="border border-gray-500 rounded-lg p-4 text-center shadow-lg">
 
-        <div key={apartment.id} className="columns-md">
-          <div className="border border-gray-500 rounded-lg">
             <PrintApartments images={apartment.image_urls} />
-            <Link to={`/dettaglio-immobile/${apartment.id}`}>
-              <h3>{apartment.titolo}</h3>
-              <p>{apartment.indirizzo_completo.split(',')[1].trim()}</p>
-              <p>{apartment.prezzo_notte}€</p>
+
+
+            <Link to={`/dettaglio-immobile/${apartment.id}`} className="block mt-2">
+              <h3 className="text-lg font-bold">{apartment.titolo}</h3>
+              <p>{apartment.indirizzo_completo.split(",")[1].trim()}</p>
+              <p className="text-xl font-semibold">{apartment.prezzo_notte}€</p>
             </Link>
+
+
             <Likes apartment={apartment} />
           </div>
         </div>
+      ))}
+    </div>
+  );
+};
 
-      ))
-      }
-    </div >
-  )
-}
+
+
 
 export default HomePage
