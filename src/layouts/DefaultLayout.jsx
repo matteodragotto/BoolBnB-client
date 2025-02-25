2f6688d232ad74cd8447b22300b39b955803e152
galdoben <galdobenedetta@gmail.com>
2025-02-25 13:17:32 +0100

    feat: add-footer

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 
 const DefaultLayout = () => {
@@ -9,7 +10,7 @@ const DefaultLayout = () => {
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <Footer />
     </>
   )
 }
