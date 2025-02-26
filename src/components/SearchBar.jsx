611b4113f6a10e8ebcac4cc4748c6013397c439d
matteodragotto <matteodrag8@gmail.com>
2025-02-26 10:08:45 +0100

    [fix] searchbar button now is a Link

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -27,12 +27,9 @@ const SearchBar = () => {
                 </svg>
               </div>
               <input type="search" id="default-search" className="block w-100 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cerca appartamenti da affittare..." required value={searchData} onChange={(e) => setSearchData(e.target.value)} />
-              <button type="submit" className="text-white absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 search-button"><Link
-                onClick={() => searchApartments(selectedCountry)}
-                to="/search"
-              >
+              <Link type="submit" className="text-white absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 search-button" onClick={() => searchApartments(selectedCountry)} to="/search">
                 Cerca
-              </Link></button>
+              </Link>
             </div>
           </form>
 
