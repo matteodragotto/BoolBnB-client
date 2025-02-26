a14ffe16a2651971710f00b395d2e5319fbba636
matteodragotto <matteodrag8@gmail.com>
2025-02-26 11:41:04 +0100

    [feat] footer elements responsive sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
         <footer>
             <div className="container flex items-center justify-between mx-auto px-4">
 
-                <div className="flex space-x-4 w-64">
+                <div className="flex space-x-4 lg:w-64">
                     <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faFacebookF} size="2x" color="white" />
+                        <FontAwesomeIcon icon={faFacebookF} color="white" className='lg:text-3xl text-2xl' />
                     </a>
                     <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faInstagram} size="2x" color="white" />
+                        <FontAwesomeIcon icon={faInstagram} color="white" className='lg:text-3xl text-2xl' />
                     </a>
                     <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faTwitter} size="2x" color="white" />
+                        <FontAwesomeIcon icon={faTwitter} color="white" className='lg:text-3xl text-2xl' />
                     </a>
                 </div>
 
@@ -27,9 +27,9 @@ const Footer = () => {
                 </div>
 
 
-                <div className="flex items-center">
+                <div className="flex items-center sm:w-64 sm:justify-end">
                     <Link to="/assistenza">
-                        <button className="bg-gradient-to-r from-[#AA895F] to-[#708F96] text-white p-3 rounded-full mt-4 hover:scale-105 transition duration-300 w-64 border border-white">Assistenza</button>
+                        <button className="bg-gradient-to-r from-[#AA895F] to-[#708F96] text-white p-3 rounded-full hover:scale-105 transition duration-300 lg:w-64 border border-white">Assistenza</button>
                     </Link>
                 </div>
             </div>
