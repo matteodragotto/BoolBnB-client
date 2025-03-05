f541411ff1d85e8dc4897e4843d4623ce090c1c8
galdoben <galdobenedetta@gmail.com>
2025-03-05 16:55:05 +0100

    fix-responsive-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-      <div className="container flex items-center justify-between">
+      <div className="container flex justify-between">
         <Link to={'/?page=1'}><img className="p-4" src="/logo.png" alt="" /></Link>
-        <div className='add-contain flex items-center m-4'>
+        <div className='add-contain flex items-center'>
           <span className='add'><Link to={'/add'}>Affitta con Bool B&B  <FontAwesomeIcon icon={faHouse} color='white' /></Link></span>
-          <FontAwesomeIcon icon={faCircleUser} size='3x' color='white' />
+
         </div>
 
       </div>
